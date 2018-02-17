f33df088c6c021628baed2ff3a7672608e887954
AlexJerez24 <alexanjerez@gmail.com>
2018-02-16 22:58:41 -0500

    Login page start

---
+++ lands/lands/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Lands
+{
+    using Xamarin.Forms;
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ lands/lands/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace lands.ViewModels
+{
+    public class MainViewModel
+    {
+        #region ViewModels
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+
+        public class LoginViewModel
+        {
+        }
+        #endregion
+    }
+}
